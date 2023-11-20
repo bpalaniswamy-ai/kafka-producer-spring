83c19055321753582f99b5a42bad032cc309aad8
balajiPS <palaniswamybalaji64@gmail.com>
2023-11-20 16:33:08 +0530

    Creating a topic with mutilple partitions

--- src/main/java/com/example/producer/controller/ProducerController.java
+++ src/main/java/com/example/producer/controller/ProducerController.java
@@ -1,6 +1,8 @@
 package com.example.producer.controller;
 
+import com.example.producer.service.KafkaMessagePublisher;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ProducerController {
 
+    @Autowired
+    private KafkaMessagePublisher kafkaMessagePublisher;
     @GetMapping("/api/v1/publish/{message}")
     public ResponseEntity<String> publishStringMessage(@PathVariable String message){
-
-        log.info("Message from request : {}", message);
+        for (int i=0; i< 10000; i++){
+            kafkaMessagePublisher.sendMessageToTopic(message+" : "+ i);
+        }
         return ResponseEntity.ok("Published successfully!!");
     }
 }
--- src/main/java/com/example/producer/service/KafkaMessagePublisher.java
+++ src/main/java/com/example/producer/service/KafkaMessagePublisher.java
@@ -1,5 +1,21 @@
 package com.example.producer.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class KafkaMessagePublisher {
-    
+
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    public void sendMessageToTopic(String message) {
+        template.send("topic-3", message)
+                .thenAccept(result -> {
+                    log.info("message sent to topic: {}, with offset: {}", "topic-1", result.getRecordMetadata().offset());
+                });
+    }
 }
